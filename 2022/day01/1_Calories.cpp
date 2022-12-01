8ecad4536200f027eb9c42a4df8b3a7c8a5f3513
Jens <jens@sunset.fritz.box>
2022-12-01 21:52:22 +0100

    Rewrite to answer both questions for day 1.

--- 2022/day01/1_Calories.cpp
+++ 2022/day01/1_Calories.cpp
@@ -1,21 +1,44 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-using std::string;
+class Cals {
+    std::vector<int> top;
+
+    public:
+    Cals(): top {0, 0, 0} {};
+    void insert(int c)
+    {
+    	for(auto&& t : top)
+    	    if (c > t) {
+    	    	auto tmp = t;
+    	    	t = c;
+    	    	c = tmp;
+	    }
+    }
+    int get_max() { return top[0]; }
+    int get_total()
+    {
+    	int total {0};
+    	for (auto i : top)
+    	    total += i;
+    	return total;
+    }
+};
 
 int main()
 {
-    int max {0}, sum {0};
+    int sum {0};
+    Cals calories;
     for(std::string n; std::getline(std::cin, n);)
     	if (n.length() > 1)
     	    sum+=std::stoi(n);
 	else {
-	    if (sum > max)
-	    	max = sum;
+	    calories.insert(sum);
 	    sum = 0;
 	}
-    if (sum > max)
-    	max = sum;
-    std::cout << max << std::endl;
+    calories.insert(sum);
+    std::cout << "Most Calories: " << calories.get_max() << std::endl;
+    std::cout << "Top three: " << calories.get_total() << std::endl;
 }
 
