a7567cb4055fab6c544fc5b5217625a9c746fa62
Jens <jens@sunset.fritz.box>
2022-12-04 15:10:33 +0100

    refactoring part 2

--- 2022/day03/2_RucksackReorg.cpp
+++ 2022/day03/2_RucksackReorg.cpp
@@ -3,27 +3,23 @@
 #include <iostream>
 #include <set>
 #include <string>
-
-
-static void str2set(std::string &a, std::set<int> &s)
-{
-    for (auto&& i : a)
-    	s.insert(i);
-}
+#include <vector>
 
 
 int main()
 {
     auto prioSum {0};
 
-    std::string elf1, elf2, elf3;
-    while (std::cin >> elf1 >> elf2 >> elf3) {
-	std::set<int> sack1, sack2, sack3;
-	str2set(elf1, sack1);
-	str2set(elf2, sack2);
-	str2set(elf3, sack3);
-	for (auto&& item : sack1)
-	    if (sack2.count(item) && sack3.count(item))
+    std::vector<std::string> elves {"","",""};
+    while (std::cin >> elves[0] >> elves[1] >> elves[2]) {
+	std::vector<std::set<int>> rucksacks;
+	for (size_t i = 0; i < elves.size(); ++i) {
+	    rucksacks.push_back({});
+	    for (auto&& item : elves[i])
+	    	rucksacks[i].insert(item);
+	}
+	for (auto&& item : rucksacks[0])
+	    if (rucksacks[1].count(item) && rucksacks[2].count(item))
 	    	prioSum += item < 'a' ? item - 'A' + 27 : item - 'a' + 1;
     }
     std::cout << prioSum << std::endl;
